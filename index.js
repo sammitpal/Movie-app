54be03eeec66ed73ec6a4bf57d167a46bac76354
sammitpal2000 <sammit.dp.pal2000@gmail.com>
2020-10-01 22:02:01 +0530

    movies

---
+++ index.js
@@ -0,0 +1,46 @@
+const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280"
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+getMovies(APIURL);
+async function getMovies(url){
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    console.log(respData);
+    showMovies(respData.results);
+   
+}
+
+function showMovies(movies){
+    main.innerHTML="";
+    movies.forEach(movie=>{
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML=`
+        <img src="${IMGPATH+movie.poster_path}" alt="">
+    
+        <div class="movie-info">
+            <h3>${movie.title}</h3>
+            <span>${movie.vote_average}</span>
+        </div>
+        <div class="overview"><h2>Overview</h2>${movie.overview}</div>`;
+
+        main.appendChild(movieEl);
+    });
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+        getMovies(SEARCHAPI+searchTerm);
+        search.value="";
+    }
+})
